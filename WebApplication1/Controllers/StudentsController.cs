1e75e87c2cf18004e9c5800a019d4075055fd8cb
s18289 <s18289@pjwstk.edu.pl>
2020-03-27 23:49:05 +0100

    commit

---
+++ WebApplication1/Controllers/StudentsController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.DAL;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+
+        private IDbService _dbService; 
+
+       public StudentsController(IDbService dbService)
+        {
+            _dbService = dbService;
+        }
+
+        [HttpGet]
+        public IActionResult getStudents(string orderBy="firstName")
+        {
+            return Ok(_dbService.GetStudents());
+        }
+        
+        [HttpGet("{id}")]
+        public IActionResult getStudent(int id) 
+        {
+            if (id == 1)
+            {
+                return Ok ("Katarzyna");
+            }
+            else if (id == 2)
+            {
+                return Ok ("Jan");
+            }
+            return NotFound("Student was not found");
+        }
+
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            student.IndexNumber = $"s{new Random().Next(1, 20000)}";
+            return Ok(student);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult getUpdate(int id)
+        {
+            return Ok("Update complete");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult getDelete(int id)
+        {
+            return Ok("Delete complete");
+        }
+    }
+}
+
+
+
+//int age = 10;
+//var s1 = "Ala is " + age + " years old";
+//var s2 = string.Format("Ala is {0} years old", age);
+//var s3 = $"Ala is {age} years old";
---
+++ WebApplication1/DAL/MockDbService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.DAL
+{
+    public class MockDbService : IDbService
+    {
+        private static IEnumerable<Student> _students;
+
+        static MockDbService()
+        {
+            _students = new List<Student>
+            {
+                new Student { IdStudent = 1, FirstName = "Jan", LastName = "Kowalski", IndexNumber = "s15236" },
+                new Student { IdStudent = 2, FirstName = "Anna", LastName = "Malewski", IndexNumber = "s17945" },
+                new Student { IdStudent = 3, FirstName = "Andrzej", LastName = "Kotowski", IndexNumber = "s13865" }
+            };
+        }
+
+        public IEnumerable<Student> GetStudents()
+        {
+            using (var con = new SqlConnection("[Data Source=db-mssql;Initial Catalog=s18289;Integrated Security=True]"))
+            using ( var com = new SqlCommand())
+            {
+                // continue from here 3.2.3
+                com.Connection = con; //join tables here
+            }
+            return _students;
+        }
+    }
+}
